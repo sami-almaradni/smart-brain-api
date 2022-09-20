c335ef083a7df52ac94e1fde60b50604617682e7
samialmaradni97 <sami.almaradni97@gmail.com>
2022-09-20 20:55:33 +0200

    improve db configuration

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ const image = require("./controllers/image.js");
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'ec2-34-253-119-24.eu-west-1.compute.amazonaws.com',
-        port: 5432,
-        user: 'wmmdmkbeyziwdc',
-        password: '75c7f5954ee20eecaa51e16f8a90f49584c840f867e7e713d2b220ccd3897ec9',
-        database: 'd2pdmphs53smt2',
-        debug: true,
+        host:process.env.DATABASE_URL,
         ssl: { rejectUnauthorized: false }
     }
 });
